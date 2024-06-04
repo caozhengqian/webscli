a589a9580cb699cef69224d1e1b628a113085177
caozhengqian <1264419544@qq.com>
2024-06-04 10:33:17 +0800

    add 成功下载目录

--- webscliI/lib/installTemplate.js
+++ webscliI/lib/installTemplate.js
@@ -1,7 +1,21 @@
 import fse from "fs-extra";
 import path from 'node:path';
+import ora from 'ora'
 import { pathExistsSync } from 'path-exists';
-
+import {log} from '@webscli/u'
+function getCacheFilePath(targetPath,template){
+  return path.resolve(targetPath,'node_modules',template.npmName,'template')
+}
+function copyFile(targetPath,template,installDir){
+  const originFile = getCacheFilePath(targetPath,template)
+  const fileList = fse.readdirSync(originFile)
+  const spinner = ora('正在拷贝模板文件...').start();
+  fileList.map(file => {
+    fse.copySync(`${originFile}/${file}`, `${installDir}/${file}`);
+  });
+  spinner.stop();
+  log.success('模板拷贝成功');
+}
 export default function installTemplate(selectedTemplate,opts){
   const {force=false} = opts
   const {targetPath, name,template} = selectedTemplate
@@ -19,4 +33,5 @@ export default function installTemplate(selectedTemplate,opts){
   }else{
     fse.ensureDirSync(installDir)
   }
+  copyFile(targetPath,template,installDir)
 }
